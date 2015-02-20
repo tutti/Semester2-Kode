a399bdd185ca4b1f4b6a0e951329c6889ec2651e
Pål Gjerde <paalvaa@gmail.com>
2015-02-20 14:00:28 +0100

    Mandelbrot fungerer. Jobber med zoom.

--- Moro/src/Mandelbrot.java
+++ Moro/src/Mandelbrot.java
@@ -1,50 +1,62 @@
 import java.awt.Dimension;
-import java.math.BigDecimal;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
 import javax.swing.JFrame;
 
 public class Mandelbrot {
-	private static final BigDecimal TWO = new BigDecimal("2");
-	private static final BigDecimal FIVE = new BigDecimal("5");
-	public static final int MAX_ITERATIONS = 100;
-	
-	public static int numIterations(BigDecimal real, BigDecimal imag) {
-		BigDecimal creal = real.setScale(3, BigDecimal.ROUND_DOWN);
-		BigDecimal cimag = imag.setScale(3, BigDecimal.ROUND_DOWN);
-//		System.out.println(cimag);
-		BigDecimal distsq = creal.multiply(creal).add(cimag.multiply(cimag));
-//		if (distsq.compareTo(FIVE) > 0) {
-//			System.out.println(real+" "+imag+" "+0);
-//			return 0;
-//		}
-		for (int i=1; i<MAX_ITERATIONS; ++i) {
-			creal = creal.multiply(creal).subtract(cimag.multiply(cimag)).add(real);
-			cimag = creal.multiply(cimag).multiply(TWO).add(imag);
-			System.out.println("0: "+creal+" "+cimag);
-//			System.out.println("1: "+creal.multiply(cimag));
-//			System.out.println("2: "+creal.multiply(cimag).multiply(TWO));
-//			System.out.println("3: "+creal.multiply(cimag).multiply(TWO).add(imag));
-			distsq = creal.multiply(creal).add(cimag.multiply(cimag));
-			if (distsq.compareTo(FIVE) > 0) {
-//				System.out.println(real+" "+imag+" "+i);
+	public static final int MAX_ITERATIONS = 1000;
+
+	public static int numIterations(double real, double imag) {
+		double creal = real;
+		double cimag = imag;
+		double distsq = creal * creal + cimag * cimag;
+		if (distsq > 4) {
+			return 1;
+		}
+		for (int i = 1; i < MAX_ITERATIONS; ++i) {
+			double lreal = creal;
+			double limag = cimag;
+			creal = lreal * lreal - limag * limag + real;
+			cimag = lreal * limag * 2 + imag;
+			distsq = lreal * lreal + limag * limag;
+			if (distsq > 4) {
 				return i;
 			}
 		}
 		return -1;
 	}
-	
+
 	public static void main(String[] args) {
-		
+
 		JFrame vindu = new JFrame();
 		vindu.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        vindu.setLayout(null);
-		
+		vindu.setLayout(null);
+
 		MandelbrotPanel panel = new MandelbrotPanel();
 		vindu.getContentPane().add(panel);
-		
-        vindu.getContentPane().setPreferredSize(new Dimension(400, 400));
-        vindu.pack();
-        vindu.setVisible(true);
-		
+		// panel.setBoundaries(-2, -2, 2, 2);
+
+		vindu.addMouseListener(new MouseAdapter() {
+			public void mouseClicked(MouseEvent e) {
+//				System.out.print("Mouse click: ");
+//				if (e.getButton() == MouseEvent.BUTTON1)
+//					System.out.print("Left button ");
+//				else
+//					System.out.print("Other button ");
+//				System.out.println(e.getX()+" "+e.getY());
+			}
+			
+			public void mouseMoved(MouseEvent e) {
+				
+			}
+		});
+
+		vindu.getContentPane().setPreferredSize(
+				new Dimension(MandelbrotPanel.WIDTH, MandelbrotPanel.HEIGHT));
+		vindu.pack();
+		vindu.setVisible(true);
+
 	}
-	
+
 }
--- Moro/src/MandelbrotPanel.java
+++ Moro/src/MandelbrotPanel.java
@@ -1,16 +1,13 @@
 import java.awt.Color;
 import java.awt.Graphics;
-import java.math.BigDecimal;
 
+import javax.swing.JButton;
 import javax.swing.JPanel;
 
 public class MandelbrotPanel extends JPanel {
 
-	public static final int WIDTH = 400;
-	public static final int HEIGHT = 400;
-	private static final BigDecimal N_TWO = new BigDecimal("-2");
-	private static final BigDecimal N_ONE = new BigDecimal("-1");
-	private static final BigDecimal ONE = new BigDecimal("1");
+	public static final int WIDTH = 800;
+	public static final int HEIGHT = 600;
 	private static final Color[] colors = new Color[Mandelbrot.MAX_ITERATIONS];
 
 	private static final long serialVersionUID = 4496197751041193170L;
@@ -18,37 +15,37 @@ public class MandelbrotPanel extends JPanel {
 
 	public MandelbrotPanel() {
 		super();
-		setBoundaries(N_TWO, N_ONE, ONE, ONE);
+		setBoundaries(-3, -1.5, 1, 1.5);
 		setBounds(0, 0, WIDTH, HEIGHT);
-		for (int i=0; i<colors.length; ++i) {
-			colors[i] = new Color((int)(i*2.5), (int)(i*2.5), 255); // TODO: Lose dependency on max_it <= 127.
+		double color = 255;
+		for (int i = 0; i < colors.length; ++i) {
+			colors[i] = new Color(255 - (int) color, 255 - (int) (color), 255);
+			color *= 0.9;
 		}
 	}
 
-	public void setBoundaries(BigDecimal x1, BigDecimal y1, BigDecimal x2, BigDecimal y2) {
-		BigDecimal xStep = x1.abs().add(x2.abs()).divide(new BigDecimal(Integer.toString(WIDTH)));
-		BigDecimal yStep = y1.abs().add(y2.abs()).divide(new BigDecimal(Integer.toString(HEIGHT)));
-		BigDecimal xVal = x1;
-		BigDecimal yVal = y1;
-//		System.out.println(xVal+" "+yVal);
-		for (int i=0; i<WIDTH; ++i) {
-			xVal = xVal.add(xStep);
-			yVal = y1;
-			for (int j=0; j<HEIGHT; ++j) {
-				System.out.println(i+" "+j+" "+xVal+" "+yVal);
-				yVal = yVal.add(yStep);
+	public void setBoundaries(double x1, double y1, double x2, double y2) {
+		double xStep = (x2 - x1) / WIDTH;
+		double yStep = (y2 - y1) / HEIGHT;
+		double xVal = x1;
+		double yVal = y1;
+		for (int i = 0; i < WIDTH; ++i) {
+			for (int j = 0; j < HEIGHT; ++j) {
+				xVal = x1 + i * xStep;
+				yVal = y1 + j * yStep;
 				pixels[i][j] = Mandelbrot.numIterations(xVal, yVal);
 			}
 		}
-//		System.out.println(xVal+" "+yVal);
 	}
 
 	public void paintComponent(Graphics g) {
-		for (int x=0; x<WIDTH; ++x) {
-			for (int y=0; y<HEIGHT; ++y) {
+		for (int x = 0; x < WIDTH; ++x) {
+			for (int y = 0; y < HEIGHT; ++y) {
 				int iterations = pixels[x][y];
-				if (iterations == -1) g.setColor(Color.BLACK);
-				else g.setColor(colors[iterations]);
+				if (iterations == -1)
+					g.setColor(Color.BLACK);
+				else
+					g.setColor(colors[iterations]);
 				g.fillRect(x, y, 1, 1);
 			}
 		}
